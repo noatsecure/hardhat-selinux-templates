def4f98b465258721020c179e1b02f136cce07a9
noatsecure <noatsecure@protonmail.com>
2021-12-29 11:58:17 -0700

    Changed placeholder from 'NAME' to 'MODULE'

--- apps/template.sh
+++ apps/template.sh
@@ -13,8 +13,8 @@ fi
 
 if [ $# -eq 1 ]; then
 	if [ "$1" = "--update" ] ; then
-		time=`ls -l --time-style="+%x %X" NAME.te | awk '{ printf "%s %s", $6, $7 }'`
-		rules=`ausearch --start $time -m avc --raw -se NAME`
+		time=`ls -l --time-style="+%x %X" MODULE.te | awk '{ printf "%s %s", $6, $7 }'`
+		rules=`ausearch --start $time -m avc --raw -se MODULE`
 		if [ x"$rules" != "x" ] ; then
 			echo "Found avc's to update policy with"
 			echo -e "$rules" | audit2allow -R
@@ -22,7 +22,7 @@ if [ $# -eq 1 ]; then
 			read ANS
 			if [ "$ANS" = "y" -o "$ANS" = "Y" ] ; then
 				echo "Updating policy"
-				echo -e "$rules" | audit2allow -R >> NAME.te
+				echo -e "$rules" | audit2allow -R >> MODULE.te
 				# Fall though and rebuild policy
 			else
 				exit 0
@@ -42,17 +42,17 @@ fi
 
 echo "Building and Loading Policy"
 set -x
-make -f /usr/share/selinux/devel/Makefile NAME.pp || exit
-/usr/sbin/semodule -i NAME.pp
+make -f /usr/share/selinux/devel/Makefile MODULE.pp || exit
+/usr/sbin/semodule -i MODULE.pp
 
 # Generate a man page off the installed module
-sepolicy manpage -p . -d NAME_t
+sepolicy manpage -p . -d MODULE_t
 
 # Fixing the file and directory contexts
-/sbin/restorecon -F -R -v /usr/bin/NAME
-/sbin/restorecon -F -R -v /usr/sbin/NAME
-/sbin/restorecon -F -R -v /etc/NAME
+/sbin/restorecon -F -R -v /usr/bin/MODULE
+/sbin/restorecon -F -R -v /usr/sbin/MODULE
+/sbin/restorecon -F -R -v /etc/MODULE
 
 # Generate a rpm package for the newly generated policy
 pwd=$(pwd)
-rpmbuild --define "_sourcedir ${pwd}" --define "_specdir ${pwd}" --define "_builddir ${pwd}" --define "_srcrpmdir ${pwd}" --define "_rpmdir ${pwd}" --define "_buildrootdir ${pwd}/.build"  -ba NAME_selinux.spec
+rpmbuild --define "_sourcedir ${pwd}" --define "_specdir ${pwd}" --define "_builddir ${pwd}" --define "_srcrpmdir ${pwd}" --define "_rpmdir ${pwd}" --define "_buildrootdir ${pwd}/.build"  -ba MODULE_selinux.spec
